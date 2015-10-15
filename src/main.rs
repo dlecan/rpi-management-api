8ca1bebcf435b50dbf1ef152e7148e90c7dc97d5
Damien Lecan <dev@dlecan.com>
2015-10-15 12:04:14 +0200

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ extern crate iron;
 extern crate rustc_serialize;
 extern crate valico;
 
-use hyper::status::StatusCode;
 use iron::Iron;
 use rustless::{
     Application, Api, Nesting, Versioning
 };
 use rustless::server::header::AccessControlAllowOrigin;
+use rustless::server::status::StatusCode;
 use rustc_serialize::json::ToJson;
 use std::process::Command;
 
@@ -28,7 +28,7 @@ fn main() {
 
     api.post("shutdown", |endpoint| {
         endpoint.handle(|mut client, params| {
-            println!("Shutdown requested");
+            println!("Shutdown requested...");
             client.set_header(AccessControlAllowOrigin::Any);
 
             let output = Command::new("shutdown")
@@ -36,9 +36,20 @@ fn main() {
                      .arg("now")
                      .output()
                      .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
-            let str_stdout = String::from_utf8(output.stdout).unwrap();
-            client.text(str_stdout)
-            // client.empty()
+
+            if output.status.success() {
+              let str_stdout = String::from_utf8(output.stdout).unwrap();
+              println!("Shutdown accepted: {}", str_stdout);
+
+              client.set_status(StatusCode::NoContent);
+              client.empty()
+            } else {
+              let str_stderr = String::from_utf8(output.stderr).unwrap();
+              println!("Shutdown rejected: {}", str_stderr);
+
+              client.unauthorized();
+              client.text(str_stderr)
+            }
         })
     });
   });
