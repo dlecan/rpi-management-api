fb01d2a8c2d7c433b8499ff0970c813417157a42
Damien Lecan <dev@dlecan.com>
2015-10-18 21:12:18 +0200

    Fix indent size to standard 4 spaces indent

--- src/main.rs
+++ src/main.rs
@@ -15,47 +15,47 @@ use std::process::Command;
 
 fn main() {
 
-  println!("Starting RPI Management ...");
+    println!("Starting RPI Management ...");
 
-  let api = Api::build(|api| {
+    let api = Api::build(|api| {
 
-    api.get("ping", |endpoint| {
-      endpoint.handle(|mut client, params| {
-        client.set_header(AccessControlAllowOrigin::Any);
-        client.empty()
-      })
+        api.get("ping", |endpoint| {
+            endpoint.handle(|mut client, params| {
+                client.set_header(AccessControlAllowOrigin::Any);
+                client.empty()
+            })
+        });
+
+        api.post("shutdown", |endpoint| {
+            endpoint.handle(|mut client, params| {
+                println!("Shutdown requested...");
+                client.set_header(AccessControlAllowOrigin::Any);
+
+                let output = Command::new("shutdown")
+                    .arg("-h")
+                    .arg("now")
+                    .output()
+                    .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+
+                if output.status.success() {
+                    let str_stdout = String::from_utf8(output.stdout).unwrap();
+                    println!("Shutdown accepted: {}", str_stdout);
+
+                    client.set_status(StatusCode::NoContent);
+                    client.empty()
+                } else {
+                    let str_stderr = String::from_utf8(output.stderr).unwrap();
+                    println!("Shutdown rejected: {}", str_stderr);
+
+                    client.unauthorized();
+                    client.text(str_stderr)
+                }
+            })
+        });
     });
 
-    api.post("shutdown", |endpoint| {
-        endpoint.handle(|mut client, params| {
-            println!("Shutdown requested...");
-            client.set_header(AccessControlAllowOrigin::Any);
+    let app = Application::new(api);
 
-            let output = Command::new("shutdown")
-                     .arg("-h")
-                     .arg("now")
-                     .output()
-                     .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
-
-            if output.status.success() {
-              let str_stdout = String::from_utf8(output.stdout).unwrap();
-              println!("Shutdown accepted: {}", str_stdout);
-
-              client.set_status(StatusCode::NoContent);
-              client.empty()
-            } else {
-              let str_stderr = String::from_utf8(output.stderr).unwrap();
-              println!("Shutdown rejected: {}", str_stderr);
-
-              client.unauthorized();
-              client.text(str_stderr)
-            }
-        })
-    });
-  });
-
-  let app = Application::new(api);
-
-  Iron::new(app).http("localhost:4000").unwrap();
+    Iron::new(app).http("localhost:4000").unwrap();
 
 }
